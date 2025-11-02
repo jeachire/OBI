9a50d6a15e002197d164343cb375d1e28bf5306d
jeachire <jeachire@uc.cl>
2025-11-02 01:58:04 -0300

    02-11-2025

--- R/.qllogis.R
+++ R/.qllogis.R
@@ -1,5 +1,3 @@
-# Helpers for log-logistic (scale = eta > 0, shape = xi > 0)
-# F(x) = a/(1+a), a = (x/eta)^xi;  Q(p) = eta * (p/(1-p))^(1/xi)
 .qllogis <- function(p, scale, shape) {
   eta <- scale; xi <- shape
   if (!(eta > 0 && xi > 0)) stop("Log-logistic requires scale > 0 and shape > 0.")
